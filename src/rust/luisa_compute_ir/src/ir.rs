90f7db8d8887db1dfe3f90561db95628af480cbf
ACMLCZH <965503066@qq.com>
2023-09-02 18:16:59 +0800

    Update ir.rs

--- src/rust/luisa_compute_ir/src/ir.rs
+++ src/rust/luisa_compute_ir/src/ir.rs
@@ -1041,7 +1041,7 @@ extern "C" fn eq_impl<T: UserNodeData>(a: *const u8, b: *const u8) -> bool {
 }
 
 fn type_id_u64<T: UserNodeData>() -> u64 {
-    unsafe { std::mem::transmute(TypeId::of::<T>()) }
+    unsafe { std::mem::transmute_copy::<TypeId, u64>(TypeId::of::<T>()) }
 }
 
 pub fn new_user_node<T: UserNodeData>(pools: &CArc<ModulePools>, data: T) -> NodeRef {
